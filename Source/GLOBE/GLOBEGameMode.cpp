f325067d42a4832933391c64fd431521c99a7977
metatomato <metatomato@iglou.gl>
2014-11-04 21:40:17 +0100

    GLOBE project launch

---
+++ Source/GLOBE/GLOBEGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GLOBE.h"
+#include "GLOBEGameMode.h"
+
+
+AGLOBEGameMode::AGLOBEGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/GLOBE/GLOBEGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GLOBEGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBE_API AGLOBEGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
